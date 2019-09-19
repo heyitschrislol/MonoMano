22830ba8232d09f7da0a75b33c160f7112915f40
Chris Hendl <Christopher.Hendl@ibm.com>
2019-09-19 15:38:19 -0500

    made some changes

--- src/application/front/InputManager.java
+++ src/application/front/InputManager.java
@@ -1,9 +1,11 @@
 package application.front;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
+import application.back.AssetManager;
 import application.back.enums.ID;
-import application.front.objects.GameObject;
+import application.front.objects.*;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
@@ -12,42 +14,70 @@ public class InputManager {
 
 	public Handler handler;
 	public static ArrayList<String> input;
-
+	public int lastpress;
+	
 	public InputManager(Handler handler) {
 		this.handler = handler;
+		input = new ArrayList<>();
 	}
 
 	public void keyPress(KeyEvent e) {
 		String key = e.getCode().toString();
-		for (int i = 0; i < handler.object.size(); i++) {
-			GameObject tempObject = handler.object.get(i);
-			if (tempObject.getID() == ID.PLAYER) {
-				if (key.contains("UP") || key.equals("W"))
-					tempObject.setVelY(-5);
-				if (key.contains("DOWN") || key.equals("S"))
-					tempObject.setVelY(5);
-				if (key.contains("LEFT") || key.equals("A"))
-					tempObject.setVelX(-5);
-				if (key.contains("RIGHT") || key.equals("D"))
-					tempObject.setVelX(5);
+		for (GameObject temp : handler.object) {
+			if (temp.getID() == ID.PLAYER) {
+				if (key.contains("DOWN")) {
+					if (!input.contains("DOWN"))input.add("DOWN");
+					if (input.contains(key)) {
+//						temp.setVelY(5);
+					}
+				}
+				if (key.contains("UP")) {
+					if (!input.contains("UP"))input.add("UP");
+					if (input.contains(key)) {
+//						temp.setVelY(-5);
+					}
+				}
+				if (key.contains("LEFT")) {
+					if (!input.contains("LEFT"))input.add("LEFT");
+					if (input.contains(key)) {
+//						temp.setVelX(-5);
+					}
+				}
+				if (key.contains("RIGHT")) {
+					if (!input.contains("RIGHT"))input.add("RIGHT");
+					if (input.contains(key)) {
+//						temp.setVelX(5);
+					}
+				}
 			}
-
 		}
 	}
 
 	public void keyRelease(KeyEvent e) {
 		String key = e.getCode().toString();
-		for (int i = 0; i < handler.object.size(); i++) {
-			GameObject tempObject = handler.object.get(i);
-			if (tempObject.getID() == ID.PLAYER) {
-				if (key.contains("UP") || key.equals("W"))
-					tempObject.setVelY(0);
-				if (key.contains("DOWN") || key.equals("S"))
-					tempObject.setVelY(0);
-				if (key.contains("LEFT") || key.equals("A"))
-					tempObject.setVelX(0);
-				if (key.contains("RIGHT") || key.equals("D"))
-					tempObject.setVelX(0);
+		for (GameObject temp : handler.object) {
+			if (temp.getID() == ID.PLAYER) {
+				if (key.contains("DOWN")) {
+					if (input.contains("DOWN"))input.remove("DOWN");
+					temp.setImage(AssetManager.returnIdles()[0]);
+					temp.setVelY(0);
+				}
+				if (key.contains("UP")) {
+					if (input.contains("UP"))input.remove("UP");
+					temp.setImage(AssetManager.returnIdles()[1]);
+					temp.setVelY(0);
+				}
+				
+				if (key.contains("LEFT")) {
+					if (input.contains("LEFT"))input.remove("LEFT");
+					temp.setImage(AssetManager.returnIdles()[2]);
+					temp.setVelX(0);
+				}
+				if (key.contains("RIGHT")) {
+					if (input.contains("RIGHT"))input.remove("RIGHT");
+					temp.setImage(AssetManager.returnIdles()[3]);
+					temp.setVelX(0);
+				}
 			}
 			
 		}
